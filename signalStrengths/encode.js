26ad3b78d0c43ccf87710527deed7b6184203ffe
4rzael <maxime.agor@epitech.eu>
2015-10-01 12:46:38 +0200

    deleted a console.log

--- signalStrengths/encode.js
+++ signalStrengths/encode.js
@@ -17,7 +17,6 @@ module.exports = function encode(measurement){
 	    var delta_protobuf_based_buffer = encodeProtoDelta(shrinkedMessage);
 
         zlib.deflate(delta_protobuf_based_buffer, function(err, buffer){
-        	console.log(buffer);
             if(err) reject(err); else resolve(buffer.toString('base64'));
         });
     });
